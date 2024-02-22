333f20197d3f68751822af17fa011377211b60e4
Sureya Sathiamoorthi <catchsureya@gmail.com>
2024-02-22 10:25:41 +0000

    split rabbit code into producer and consumer

---
+++ sample_scripts/mq_producer.py
@@ -0,0 +1,5 @@
+from sample_scripts.rabbit_client import RabbitMQ
+
+if __name__ == '__main__':
+    mq = RabbitMQ()
+    mq.publish()
--- sample_scripts/rabbit_client.py
+++ sample_scripts/rabbit_client.py
@@ -2,20 +2,29 @@ from rabbitmq_client import RMQConsumer, ConsumeParams, QueueParams
 import pika
 
 
-def message_handler(channel, method, properties, body):
-    print("Received message:", body.decode())
-
+def callback(ch, method, properties, body):
+    print(f" [x] Received {body}")
 
 class RabbitMQ:
     def __init__(self):
         self.queue_name = 'TEST_QUEUE_1'
         self.connection_params = pika.ConnectionParameters('localhost')
         self.consumer = RMQConsumer(self.connection_params)
-        self.queue_params = QueueParams(self.queue_name, durable=True)
+        self.queue_params = QueueParams(
+            self.queue_name,
+            durable=True
+        )
 
     def consume(self):
-        consume_params = ConsumeParams(self.queue_params, message_handler)
-        self.consumer.consume(consume_params)
+        with pika.BlockingConnection(self.connection_params) as connection:
+            channel = connection.channel()
+            channel.queue_declare(queue=self.queue_name)
+            channel.basic_consume(
+                queue=self.queue_name,
+                auto_ack=True,
+                on_message_callback=callback
+            )
+            channel.start_consuming()
 
     def publish(self):
         with pika.BlockingConnection(self.connection_params) as connection:
@@ -28,8 +37,3 @@ class RabbitMQ:
             )
 
         print("Message sent successfully.")
-
-
-if __name__ == '__main__':
-    mq = RabbitMQ()
-    mq.publish()
